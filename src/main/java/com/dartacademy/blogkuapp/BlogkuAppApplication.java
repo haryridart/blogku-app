eaa942975ce7165ea2aeb36b7e88409a69431dbe
Hary Ridart <hary.teguh@ui.ac.id>
2023-07-21 21:56:26 +0700

    first commit

---
+++ src/main/java/com/dartacademy/blogkuapp/BlogkuAppApplication.java
@@ -0,0 +1,13 @@
+package com.dartacademy.blogkuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogkuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogkuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dartacademy/blogkuapp/BlogkuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dartacademy.blogkuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogkuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
